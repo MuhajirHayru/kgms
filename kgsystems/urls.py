0dfba4e37e29b2ba8b8330cdfccd94d2faffc942
unknown <muhajirhayru18@gmail.com>
2026-02-28 03:03:13 -0800

    Fix token route to use TokenObtainPairView

--- kgsystems/urls.py
+++ kgsystems/urls.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,)
-
+    TokenRefreshView,
+)
 
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import CustomTokenObtainPairView
-from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -25,7 +24,7 @@ urlpatterns = [
     path('api/finance/', include('finance.urls')),
     # Students
     path('api/students/', include('students.urls')),
-     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
